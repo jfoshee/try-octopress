9cc7657d4a0daab9b06138550f8e898c43a54e4b
Jacob Foshee <jacobf@gmail.com>
2013-06-15 16:24:58 -0500

    setup for gh pages

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/try-octopress/"
+http_images_path = "/try-octopress/images"
+http_fonts_path = "/try-octopress/fonts"
+css_dir = "public/try-octopress/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
